e72de7b4db53d4c1275631ad34bfde89f79c4d33
Musa Issack <musa.m.issack@gmail.com>
2023-06-14 19:39:24 +0300

    assisnment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Musa Issack';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + 'MUSA ISSACK' + '!' 
+const shortGreeting = "Welcome, " + 'M' + '!'
